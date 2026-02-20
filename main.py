ac0eb26d6b1e623901a050b279916574626840b0
loaderofillusion <art.mizirov@mail.ru>
2026-02-20 17:25:14 +0500

    initial com

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ def index():
 
 @app.route('/odd_even')
 def odd_even():
-    return render_template('odd_even.html'  )
+    return render_template('odd_even.html')
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

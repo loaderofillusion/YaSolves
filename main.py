9871f73b3bfc5027ad13a964e6375173d72fb06d
loaderofillusion <art.mizirov@mail.ru>
2026-02-27 20:55:50 +0500

    анкета для заполнения

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from flask import *
 
 app = Flask(__name__)
-
+app.secret_key = 'supersecretkey'
 @app.route('/')
 @app.route('/promotion_image')
 def index():
     return render_template('promotion_image.html')
 
-@app.route('/odd_even')
-def odd_even():
-    return render_template('odd_even.html')
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def selection():
+    form_data = {}
+    if request.method == 'POST':
+
+        form_data = {
+            'name': request.form.get('name'),
+            'surname': request.form.get('surname'),
+            'email': request.form.get('email', ''),
+            'degree': request.form.get('degree', ''),
+            'motivation': request.form.get('motivation', ''),
+            'sex': request.form.get('sex', 'male'),
+            'directions': request.form.getlist('directions'),
+            'accept': 'accept' in request.form  # Checkbox: True/False
+        }
+
+        print("Полученные данные:", form_data)
+
+        flash('Форма отправлена! Данные сохранены.', 'success')
+        return render_template('selection.html', form_data=form_data)
+
+    return render_template('selection.html', form_data=form_data)
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

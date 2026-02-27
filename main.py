16cd6f48ef27abc01148995fe94dfd862ce1f762
loaderofillusion <artemijmizirov@gmail.com>
2026-02-27 16:05:13 +0500

    донателла

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
+import random
+
 import arcade
 
 SPEED = 4
 SCREEN_WIDTH = 960
 SCREEN_HEIGHT = 640
-SCREEN_TITLE = "Leonardo Game"
+SCREEN_TITLE = "Donatello Game"
+CELL_SIZE = 64
+
+
+class Enemy(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.texture = arcade.load_texture(":resources:images/enemies/fly.png")
+        self.scale = 0.3
+
+    def update(self, delta_time):
+        """ Отражение персонажа """
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        if self.left < CELL_SIZE or self.right > SCREEN_WIDTH - CELL_SIZE:
+            self.change_x *= -1
+        if self.bottom < CELL_SIZE or self.top > SCREEN_HEIGHT - CELL_SIZE:
+            self.change_y *= -1
 
 
 class GridGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        self.cell_size = 64
+        self.cell_size = CELL_SIZE
         self.all_sprites = arcade.SpriteList()
         self.wall_sprites = arcade.SpriteList()
+        self.enemy_sprites = arcade.SpriteList()
         # Загружаем текстуры из встроенных ресурсов
         self.stone_texture = arcade.load_texture(":resources:images/tiles/stoneCenter.png")
         self.sand_texture = arcade.load_texture(":resources:images/tiles/sandCenter.png")
         self.player_texture = arcade.load_texture(":resources:images/enemies/slimeBlue.png")
+        self.player1_texture = arcade.load_texture(":resources:images/enemies/slimePurple.png")
 
     def setup(self):
         self.grid = [[0 for _ in range(15)] for _ in range(10)]
@@ -39,15 +60,38 @@ class GridGame(arcade.Window):
                     self.wall_sprites.append(stone_sprite)
                     self.all_sprites.append(stone_sprite)
 
+        free_places = [(x, y) for x in range(1, 14) for y in range(1, 9)]
         self.player = arcade.Sprite(self.player_texture, scale=0.5)
-        x = 7 * self.cell_size + self.cell_size // 2
+        x = 12 * self.cell_size + self.cell_size // 2
         y = 5 * self.cell_size + self.cell_size // 2
         self.player.position = (x, y)
         self.all_sprites.append(self.player)
+        free_places.remove((12, 5))
+
+        self.player1 = arcade.Sprite(self.player1_texture, scale=0.5)
+        x = 2 * self.cell_size + self.cell_size // 2
+        y = 5 * self.cell_size + self.cell_size // 2
+        self.player1.position = (x, y)
+        self.all_sprites.append(self.player1)
+        free_places.remove((2, 5))
 
         self.physics_engine = arcade.PhysicsEngineSimple(
             self.player, self.wall_sprites
         )
+        self.physics_engine1 = arcade.PhysicsEngineSimple(
+            self.player1, self.wall_sprites
+        )
+
+        # Enemies
+        for _ in range(5):
+            enemy = Enemy()
+            enemy.change_x = random.randint(-4, 4)
+            enemy.change_y = random.randint(-4, 4)
+            x, y = random.choice(free_places)
+            free_places.remove((x, y))
+            enemy.position = (x * self.cell_size + self.cell_size // 2, y * self.cell_size + self.cell_size // 2)
+            self.enemy_sprites.append(enemy)
+            self.all_sprites.append(enemy)
 
     def on_draw(self):
         self.clear()
@@ -55,6 +99,13 @@ class GridGame(arcade.Window):
 
     def on_update(self, delta_time: float):
         self.physics_engine.update()
+        self.physics_engine1.update()
+        self.enemy_sprites.update()
+        check_enemy = arcade.check_for_collision_with_list(self.player, self.enemy_sprites) + \
+                      arcade.check_for_collision_with_list(self.player1, self.enemy_sprites)
+        for enemy in check_enemy:
+            self.enemy_sprites.remove(enemy)
+            self.all_sprites.remove(enemy)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.UP:
@@ -66,14 +117,27 @@ class GridGame(arcade.Window):
         elif key == arcade.key.RIGHT:
             self.player.change_x = SPEED
 
+        if key == arcade.key.W:
+            self.player1.change_y = SPEED
+        elif key == arcade.key.S:
+            self.player1.change_y = -SPEED
+        elif key == arcade.key.A:
+            self.player1.change_x = -SPEED
+        elif key == arcade.key.D:
+            self.player1.change_x = SPEED
+
     def on_key_release(self, key, modifiers):
         if key in [arcade.key.UP, arcade.key.DOWN]:
             self.player.change_y = 0
         if key in [arcade.key.LEFT, arcade.key.RIGHT]:
             self.player.change_x = 0
+        if key in [arcade.key.W, arcade.key.S]:
+            self.player1.change_y = 0
+        if key in [arcade.key.A, arcade.key.D]:
+            self.player1.change_x = 0
 
 
-def setup_game(width=960, height=640, title="Leonardo Game"):
+def setup_game(width=960, height=640, title="Donatello Game"):
     game = GridGame(width, height, title)
     game.setup()
     return game
@@ -86,3 +150,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
